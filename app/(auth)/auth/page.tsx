4a6a9816798e65f5a0b931d1f7f5736814e208d8
touraye <touraye07@gmail>
2025-01-01 16:56:16 +0000

    setup basic app layout

---
+++ app/(auth)/auth/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthPage() {
+  return (
+    <>
+      <h1>Login to your app</h1>      
+    </>
+  );
+}
---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+export default function Layout({children}: Readonly<{children: React.ReactNode;}>) {
+  return (
+      <section>
+        {children}
+    </section>
+  );
+    
+}
